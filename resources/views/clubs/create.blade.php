33a2b52e1a8edb7e8d3878011da61f5558fea5da
riadhyahyaoui94 <riadh.yahyaouui.com>
2022-10-25 23:12:52 +0100

    Fixed migration and cascade

--- resources/views/clubs/create.blade.php
+++ resources/views/clubs/create.blade.php
@@ -8,9 +8,14 @@
         <form action="{{ url('club') }}" method="post">
             {!! csrf_field() !!}
             <label>nom club</label></br>
-            <input type="text" name="nomClub" id="specialite" class="form-control"></br>
+            <input type="text" name="nomClub" id="nomClub" class="form-control"></br>
             <label>specialite</label></br>
-            <input type="text" name="specialite" id="specialite" class="form-control"></br>
+            <select class="form-select mb-5" name="specialite">
+                <option value="TWIN">TWIN</option>
+                <option value="SE">SE</option>
+                <option value="ArcTIC">ArcTIC</option>
+                <option value="NIDS">NIDS</option>
+            </select>
 
             <input type="submit" value="Save" class="btn btn-success"></br>
         </form>
